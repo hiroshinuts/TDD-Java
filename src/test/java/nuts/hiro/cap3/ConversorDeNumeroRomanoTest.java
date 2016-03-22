00032c97ef2090c3bfcfbe8921f470047b793841
Rafael Hiroshi <hiroshi_nuts@hotmail.com>
2016-03-22 02:02:07 -0300

    correção do nome da Classe de Teste

--- src/test/java/nuts/hiro/cap3/ConversorDeNumeroRomanoTest.java
+++ src/test/java/nuts/hiro/cap3/ConversorDeNumeroRomanoTest.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-public class ConversorDeNumeroRomanoTestI {
+public class ConversorDeNumeroRomanoTest {
 
 	@Test
 	public void deveEntenderOSimbolo(){
