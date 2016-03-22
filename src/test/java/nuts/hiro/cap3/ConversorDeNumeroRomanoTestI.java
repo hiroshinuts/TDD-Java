56bf63c7ad6be297d242e1ddf3e33d84cbb78fb5
Rafael Hiroshi <hiroshi_nuts@hotmail.com>
2016-03-22 01:58:07 -0300

    Termino do Capitulo 3, até a pagina 31

---
+++ src/test/java/nuts/hiro/cap3/ConversorDeNumeroRomanoTestI.java
@@ -0,0 +1,45 @@
+package nuts.hiro.cap3;
+
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ConversorDeNumeroRomanoTestI {
+
+	@Test
+	public void deveEntenderOSimbolo(){
+		ConversorDeNumeroRomano romano = new ConversorDeNumeroRomano();
+		int numero = romano.converte("I");
+		assertEquals(1,numero);
+	}
+	
+	@Test
+	public void deveEntenderOSimboloV() {
+		ConversorDeNumeroRomano romano = new ConversorDeNumeroRomano();
+		int numero = romano.converte("V");
+		assertEquals(5, numero);
+	}
+	
+	@Test
+	public void deveEntenderDoisSimbolosComoII(){
+		ConversorDeNumeroRomano romano = new ConversorDeNumeroRomano();
+		int numero = romano.converte("II");
+		assertEquals(2, numero);
+	}
+	
+	@Test
+	public void deveEntenderQuatroSimbolosDoisADoisCOMOXXII(){
+		ConversorDeNumeroRomano romano = new ConversorDeNumeroRomano();
+		int numero = romano.converte("XXII");
+		assertEquals(22, numero);
+	}
+	
+	@Test
+	public void deveEntederNumerosComoIX(){
+		ConversorDeNumeroRomano romano = new ConversorDeNumeroRomano();
+		int numero = romano.converte("IX");
+		assertEquals(9, numero);
+	}
+	
+}
